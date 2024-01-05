c0bff3bd28801f7fd683fef58eeb7fa9464d57f3
Sardar Shoaib <sardarshoaib910@gmail.com>
2024-01-05 23:20:46 +0500

    5th jan

--- src/componets/common/listGroup.jsx
+++ src/componets/common/listGroup.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ListGroup = (props) => {
     const{items, textProperty, valueProperty,selectedItem, onItemSelect}=props;
-    return <ul className="list-group">
+    return <ul className="list-group m-2">
+    <li className="list-group-item bg-dark text-white font-monospace" >SELEC GENERES FILTER</li>
     {items.map(item=><li onClick={()=>onItemSelect(item)} key={item[valueProperty]} className={item===selectedItem? "list-group-item active": "list-group-item"}>{item[textProperty]}</li>)}
-    <li className="list-group-item"></li>
   </ul>;
 };
 ListGroup.defaultProps={
