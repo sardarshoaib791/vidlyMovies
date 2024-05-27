1757a89f5eb82cc3ea59ac33194fead08e6cdb09
Sardar Shoaib <sardarshoaib910@gmail.com>
2024-05-27 20:38:54 +0500

    27th may

--- src/componets/common/pagination.jsx
+++ src/componets/common/pagination.jsx
@@ -10,7 +10,7 @@ const Pagination = props => {
 
 
     return <nav>
-    <ul className="pagination">
+    <ul className=" pagination">
     {pages.map(page=>(<li key={page} className={page===currentPage? "page-item active": "page-item"}>
       <a className="page-link" onClick={()=>onPageChange(page)}>{page}</a>
       </li>))}
