d4d3684a0515fdbcb470edd86484786907a308e3
Sardar Shoaib <sardarshoaib910@gmail.com>
2024-05-31 21:50:05 +0500

    31 may 2024

--- src/componets/common/input.jsx
+++ src/componets/common/input.jsx
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-const Input = ({name, lable,value, onChange, error}) => {
-    return (  <div className="form-group">
-                        <lable htmlFor={name}>{lable}</lable>
-                        <input
-                            name={name}
-                            value={value}
-                            onChange={onChange} 
-                            id={name} 
-                            type="text" className="form-control" />
-                            {error && <div className="alert alert-danger">{error}</div>}
-                    </div> );
-}
- 
+const Input = ({ type, name, lable, value, onChange, error }) => {
+  return (
+    <div className="form-group">
+      <lable htmlFor={name}>{lable}</lable>
+      <input
+        name={name}
+        value={value}
+        onChange={onChange}
+        id={name}
+        type={type}
+        className="form-control"
+      />
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
 export default Input;
--- src/componets/loginForm.jsx
+++ src/componets/loginForm.jsx
@@ -1,55 +1,35 @@
-import React, { Component } from 'react';
-import Input from './common/input';
-import Form from './common/form';
-import  Joi, { abort }  from 'joi-browser';
-import { error } from 'jquery';
-
-
+import React, { Component } from "react";
+import Form from "./common/form";
+import Joi, { abort } from "joi-browser";
+import { error } from "jquery";
 
 class LoginForm extends Form {
-    state = {
-        data: { username: "", password: "" },
-        errors: {}
-    }
+  state = {
+    data: { username: "", password: "" },
+    errors: {},
+  };
 
-    schema = {
-        username : Joi.string().required().label('Username'),
-        password : Joi.string().required().label('Password'),
-    }
-    
-    
-    
-    doSubmit=()=>{
-        console.log("submitted")
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
 
-    }
-    
-    
-    render() {
-        const{data, errors} = this.state;
-        return (
-            <div>
-                <h1>Login</h1>
-                <form onSubmit={this.handleSubmit}>
-                <Input
-                lable="Username"
-                 name="username"
-                 value={data.username}
-                onChange={this.handleChange} 
-                error={errors.username}    
-                />
-                <Input
-                lable="Password"
-                 name="password"
-                 value={data.password}
-                onChange={this.handleChange} 
-                error={errors.password}    
-                />   
-                  {this.renderButton("Login")}
-                </form>
-            </div>
-        );
-    }
+  doSubmit = () => {
+    console.log("submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default LoginForm;
